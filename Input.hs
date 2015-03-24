f4370e97b157c9deba00b7079a7ab345f1e3c536
Erik Peldan <epe@trifork.com>
2015-03-24 21:36:55 +0100

    Configure app to quit when pressed 'q'

--- Input.hs
+++ Input.hs
@@ -2,7 +2,7 @@ module Input where
 import Data.IORef (IORef, modifyIORef')
 
 import qualified Graphics.UI.GLUT as GLUT
-import Graphics.UI.GLUT (Key, KeyState, Modifiers, Position, 
+import Graphics.UI.GLUT (Key(..), KeyState(..), Modifiers, Position, 
                          leaveMainLoop, KeyboardMouseCallback)
 
 data Event = Event Key KeyState Modifiers GLUT.Position deriving (Show, Eq)
@@ -11,7 +11,10 @@ type InputIORef = IORef [Event]
 
 handleSpecial :: [Event] -> IO ()
 handleSpecial [] = return ()
-handleSpecial xs = putStrLn (show xs) >> leaveMainLoop
+handleSpecial xs = mapM_ handleSpecial' xs
+    where
+    handleSpecial' (Event (Char 'q') Down _ _) = leaveMainLoop
+    handleSpecial' x = putStrLn (show x) >> return ()
 
 callback :: InputIORef -> KeyboardMouseCallback
 callback iorf key keyState mods pos = modifyIORef' iorf ( event : )
