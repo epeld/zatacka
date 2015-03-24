825032ff411a26a3feca3956b0d98c96ab9c0f46
Erik Peldan <epe@trifork.com>
2015-03-24 21:57:37 +0100

    Move setup to display module

--- Display.hs
+++ Display.hs
@@ -1,5 +1,5 @@
-module Display (callback, Displayer) where
-import Graphics.UI.GLUT 
+module Display (callback, Displayer, setup) where
+import Graphics.UI.GLUT as GLUT
 import Graphics.Rendering.OpenGL 
 
 import GLUTContext as GLUTC
@@ -11,3 +11,32 @@ callback ctx d = state ctx >>= runDisplayer d
 
 runDisplayer :: Displayer a -> a -> IO ()
 runDisplayer = ($)
+
+
+setup :: String -> IO ()
+setup title = do
+    setGLUTParameters
+    createWindow title
+    setGLParameters
+
+setGLParameters :: IO ()
+setGLParameters = do
+    clearColor $= Color4 0 0.0 0 (0 :: GLclampf)
+    clear [ColorBuffer]
+    drawBuffer             $= BackBuffers
+    viewport               $= ((Position 0 0), Size 640 480)
+    matrixMode             $= Projection
+    --loadIdentity
+    --perspective 70.0 (640/480) 10.0  4000.0
+    ortho (-500) 500 (-500) 500 (-500) 500
+    matrixMode             $= Modelview 0
+    loadIdentity
+    cursor                 $= None
+
+setGLUTParameters :: IO ()
+setGLUTParameters = do
+    putStrLn "- Display mode settings"
+    initialDisplayMode $= [DoubleBuffered, RGBAMode]
+    putStrLn "- Window size"
+    initialWindowSize  $= Size 640 480
+
--- Main.hs
+++ Main.hs
@@ -1,10 +1,9 @@
 import Graphics.UI.GLUT as GLUT
 
 import qualified GLUTContext as GLUTC
---import qualified Render
---import qualified Mutate
 import RotatingQuad as R
 import Activate
+import Display
 
 data GameInput = GameInput
 data GameState = GameState
@@ -20,14 +19,8 @@ initialize :: IO ()
 initialize = do
     GLUT.initialize "Super Game" []
 
-    putStrLn "Setting Paramters"
-    setGLUTParameters
-
-    putStrLn "Creating Window"
-    createWindow "Cool game"
-
-    putStrLn "Setting Rendering Parameters"
-    setRenderingParameters
+    putStrLn "Setting up Graphics.."
+    Display.setup "Zatachoo"
 
     putStrLn "Creating Context"
     ctx <- context 0
@@ -36,26 +29,3 @@ initialize = do
     activate ctx R.transform R.display
 
 
-setGLUTParameters :: IO ()
-setGLUTParameters = do
-    putStrLn "- Display mode settings"
-    initialDisplayMode $= [DoubleBuffered, RGBAMode]
-    putStrLn "- Window size"
-    initialWindowSize  $= Size 640 480
-
-
-setRenderingParameters :: IO ()
-setRenderingParameters = do
-    clearColor $= Color4 0 0.0 0 (0 :: GLclampf)
-    clear [ColorBuffer]
-    drawBuffer         $= BackBuffers
-    --viewport               $= ((Position 0 0), Size 640 480)
-    --matrixMode             $= Projection
-    --loadIdentity
-    --perspective 70.0 (640/480) 10.0  4000.0
-    matrixMode             $= Modelview 0
-    loadIdentity
-    --depthFunc              $= Just Less
-    --texture Texture2D  $=  Enabled
-    --cullFace               $= Just Front
-    cursor                 $= None
