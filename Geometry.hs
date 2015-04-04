d74d442505bf6ca6d504141a1a74d48702edab43
Erik Peldan <epe@trifork.com>
2015-04-04 23:28:37 +0200

    cleanup

--- Geometry.hs
+++ Geometry.hs
@@ -11,6 +11,7 @@ import Checkpoint
 
 import qualified LinearUtils as LU
 
+-- Note: arc info is currently not enough
 data GeometryA a = Line { _start :: V2 a, _end :: V2 a } | Arc { _centre :: V2 a, _start :: V2 a, _end :: V2 a }
 $(makeLenses ''GeometryA)
 
@@ -20,8 +21,10 @@ type Turn = Direction
 type DirectionChanges = TimeSeries (Maybe Turn)
 type TurnEvent = Event (Maybe Turn)
 
+
 geometries :: [TurnEvent] -> State Checkpoint [Geometry]
 geometries = sequence . fmap geometry
+
 
 geometry :: TurnEvent -> State Checkpoint Geometry
 geometry ev = do
@@ -33,17 +36,26 @@ geometry ev = do
     heading .= endHeading ev cp
     return geom
 
-omega Direction.Left dt = -dt
-omega Direction.Right dt = dt
+
+line :: Checkpoint -> FloatType -> Geometry
+line cp d = Line { _start = cp ^. position, _end = cp ^. position + cp ^. heading ^* d }
+
+
+arc :: Checkpoint -> FloatType -> Geometry
+arc cp omga = 
+    Arc { _centre = c, _start = cp ^. position, _end = c - LU.rotation omga !* cv }
+    where 
+    cv = LU.centre (cp ^. heading) omga
+    c = (cp ^. position) + c
+
+
+
 
 endHeading ev cp = e (ev ^. content)
     where
     e Nothing = cp ^. heading
     e (Just d) = LU.rotation (omega d $ ev ^. duration) !* (cp ^. heading)
 
-line cp d = Line { _start = cp ^. position, _end = cp ^. position + cp ^. heading ^* d }
-arc cp omga = 
-    Arc { _centre = c, _start = cp ^. position, _end = c - LU.rotation omga !* cv }
-    where 
-    cv = LU.centre (cp ^. heading) omga
-    c = (cp ^. position) + c
+
+omega Direction.Left dt = -dt
+omega Direction.Right dt = dt
