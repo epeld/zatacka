43ec9ef29fb663cbb6ea4f89699314745ee657f0
Erik Peldan <epe@trifork.com>
2015-11-21 13:54:08 +0100

    Create more signals..

--- src/Signal.hs
+++ src/Signal.hs
@@ -1,29 +1,67 @@
 module Signal where
 import Graphics.UI.GLUT.Callbacks.Global (addTimerCallback)
 import Graphics.UI.GLUT.State (elapsedTime)
-import Graphics.Rendering.OpenGL.GL.StateVar (get)
+import qualified Graphics.Rendering.OpenGL.GL.StateVar as StateVar
 
+import Control.Monad
 import Data.IORef
 
-data Signal a = Signal { updateSignal :: IO (a, Maybe a) }
+type Listener a = a -> IO ()
+
+data Signal a = Signal { get :: IO a, set :: a -> IO (), subscribe :: Listener a -> IO () }
 
 
-timerSignal :: Int -> IO (Signal Int)
-timerSignal ms = do
-    ref <- newIORef 0
+atomicCons ref x = atomicModifyIORef ref (\xs -> (x : xs, xs))
+atomicCons_ ref x = atomicModifyIORef ref (\xs -> (x : xs, ()))
 
-    let runAndReschedule :: IO ()
-        runAndReschedule = do 
-            time <- get elapsedTime
-            writeIORef ref time
-            addTimerCallback ms runAndReschedule 
 
-    let update :: IO (Int, Maybe Int)
-        update = do
-            time <- readIORef ref
-            return (time, Just time)
+constant :: a -> IO (Signal a)
+constant initial = do
+    ref <- newIORef initial
+    lref <- newIORef []
+
+    let sets a = writeIORef ref a >> readIORef lref >>= mapM_ ($ a) 
+    let gets = readIORef ref
+    let subscribes = atomicCons_ lref
+
+    return (Signal gets sets subscribes)
+        
+
+timer :: Int -> IO (Signal Int)
+timer ms = do
+    signal <- constant 0
+
+    let runAndReschedule = do
+            time <- StateVar.get elapsedTime
+            set signal time
+            addTimerCallback ms runAndReschedule
 
     runAndReschedule
 
-    return (Signal update)
-        
+    return signal
+
+
+delayed :: Int -> Signal a -> IO (Signal a)
+delayed n s = do
+    v <- get s
+
+    signal <- constant v
+    ref <- newIORef []
+
+    subscribe s $ \a -> do
+        v <- atomicModifyIORef ref $ \as -> 
+            let
+                as' = take n (a : as)
+            in
+                (as', nth n as')
+
+        case v of
+            Just val -> set signal val
+            Nothing -> return ()
+       
+    return signal
+
+
+nth _ [] = Nothing
+nth 0 [x] = Just x
+nth n (x : xs) = nth (n-1) xs
